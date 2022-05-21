fefa5c9f0a90aeac51f593058856f682d00f84a5
DizzyH <38224747+DizzyHS@users.noreply.github.com>
2022-05-21 16:37:23 -0300

    deixando os json mais clean

--- images_generator/image_generator.py
+++ images_generator/image_generator.py
@@ -74,7 +74,8 @@ def generate_images():
         colors_note = [(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255),(255,255,255)]
         average_note = (24,249,248)
 
-        soma2 = float(series['series'][song]['note1'])+float(series['series'][song]['note2'])+float(series['series'][song]['note3'])+float(series['series'][song]['note4'])+float(series['series'][song]['note5'])+float(series['series'][song]['note6'])+float(series['series'][song]['note7'])+float(series['series'][song]['note8'])
+        soma2 = float(series['series'][song]['notes'][0])+float(series['series'][song]['notes'][1])+float(series['series'][song]['notes'][2])+float(series['series'][song]['notes'][3])+float(series['series'][song]['notes'][4])+float(series['series'][song]['notes'][5])+float(series['series'][song]['notes'][6])+float(series['series'][song]['notes'][7])
+   
         random_number = -30
         random_number2 = 51
         border = (2, 2, 2, 2)
@@ -82,7 +83,7 @@ def generate_images():
         text.text((1600,62),"Average: {}".format(round(soma2/8, 2)), fill=average_note, font=font36, anchor='mm')
         #1
         text.text((1661+random_number2,70+190+random_number),series["participants"][0], fill=colors[0], font=font242, anchor='mm')
-        text.text((1661+random_number2,70+25+190+random_number),series["series"][song]["note1"], fill=colors_note[0], font=font242, anchor='mm')
+        text.text((1661+random_number2,70+25+190+random_number),series["series"][song]["notes"][0], fill=colors_note[0], font=font242, anchor='mm')
 
         par1Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][0]))
         par1Img = par1Img.convert('RGB')
@@ -92,7 +93,7 @@ def generate_images():
         
         #2
         text.text((1661+random_number2,70+190+190+random_number),series["participants"][1], fill=colors[1], font=font242, anchor='mm')
-        text.text((1661+random_number2,70+25+190+190+random_number),series["series"][song]["note2"], fill=colors_note[1], font=font242, anchor='mm')
+        text.text((1661+random_number2,70+25+190+190+random_number),series["series"][song]["notes"][1], fill=colors_note[1], font=font242, anchor='mm')
         
         par2Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][1]))
         par2Img = par2Img.convert('RGB')
@@ -101,7 +102,7 @@ def generate_images():
         image.paste(par2Img, (1661-14,70+190+190+random_number-142))
         #3
         text.text((1661+random_number2,70+190+190+190+random_number),series["participants"][2], fill=colors[2], font=font242, anchor='mm')
-        text.text((1661+random_number2,70+25+190+190+190+random_number),series["series"][song]["note3"], fill=colors_note[2], font=font242, anchor='mm')
+        text.text((1661+random_number2,70+25+190+190+190+random_number),series["series"][song]["notes"][2], fill=colors_note[2], font=font242, anchor='mm')
         
         par3Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][2]))
         par3Img = par3Img.convert('RGB')
@@ -110,7 +111,7 @@ def generate_images():
         image.paste(par3Img, (1661-14,70+190+190+190+random_number-142))
         #4
         text.text((1661+random_number2,70+190+190+190+190+random_number),series["participants"][3], fill=colors[3], font=font242, anchor='mm')
-        text.text((1661+random_number2,70+25+190+190+190+190+random_number),series["series"][song]["note4"], fill=colors_note[3], font=font242, anchor='mm')
+        text.text((1661+random_number2,70+25+190+190+190+190+random_number),series["series"][song]["notes"][3], fill=colors_note[3], font=font242, anchor='mm')
         
         par4Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][3]))
         par4Img = par4Img.convert('RGB')
@@ -119,7 +120,7 @@ def generate_images():
         image.paste(par4Img, (1661-14,70+190+190+190+190+random_number-142))
         #5
         text.text((1503+random_number2,70+190+random_number),series["participants"][4], fill=colors[4], font=font242, anchor='mm')
-        text.text((1503+random_number2,70+25+190+random_number),series["series"][song]["note5"], fill=colors_note[4], font=font242, anchor='mm')
+        text.text((1503+random_number2,70+25+190+random_number),series["series"][song]["notes"][4], fill=colors_note[4], font=font242, anchor='mm')
         
         par5Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][4]))
         par5Img = par5Img.convert('RGB')
@@ -128,7 +129,7 @@ def generate_images():
         image.paste(par5Img, (1503-14, 70+190+random_number-142))
         #6
         text.text((1503+random_number2,70+190+190+random_number),series["participants"][5], fill=colors[5], font=font242, anchor='mm')
-        text.text((1503+random_number2,70+25+190+190+random_number),series["series"][song]["note6"], fill=colors_note[5], font=font242, anchor='mm')
+        text.text((1503+random_number2,70+25+190+190+random_number),series["series"][song]["notes"][5], fill=colors_note[5], font=font242, anchor='mm')
         
         par6Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][5]))
         par6Img = par6Img.convert('RGB')
@@ -138,7 +139,7 @@ def generate_images():
         
         #7
         text.text((1503+random_number2,70+190+190+190+random_number),series["participants"][6], fill=colors[6], font=font242, anchor='mm')
-        text.text((1503+random_number2,70+25+190+190+190+random_number),series["series"][song]["note7"], fill=colors_note[6], font=font242, anchor='mm')
+        text.text((1503+random_number2,70+25+190+190+190+random_number),series["series"][song]["notes"][6], fill=colors_note[6], font=font242, anchor='mm')
         
         par7Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][6]))
         par7Img = par7Img.convert('RGB')
@@ -147,7 +148,7 @@ def generate_images():
         image.paste(par7Img, (1503-14, 70+190+190+190+random_number-142))
         #8
         text.text((1503+random_number2,70+190+190+190+190+random_number),series["participants"][7], fill=colors[7], font=font242, anchor='mm')
-        text.text((1503+random_number2,70+25+190+190+190+190+random_number),series["series"][song]["note8"], fill=colors_note[7], font=font242, anchor='mm')
+        text.text((1503+random_number2,70+25+190+190+190+190+random_number),series["series"][song]["notes"][7], fill=colors_note[7], font=font242, anchor='mm')
 
         par8Img = Image.open(PARTICIPANTS_PATH+"/{}.png".format(series["participants"][7]))
         par8Img = par8Img.convert('RGB')
